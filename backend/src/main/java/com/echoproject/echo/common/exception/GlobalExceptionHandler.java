a2382df64ec2fe10e4488609bb7d17c7825ba7ab
YounesDjelloul <younesdjelloul14@gmail.com>
2025-12-07 20:51:51 +0100

    add global exception handler for validation errors

--- backend/src/main/java/com/echoproject/echo/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/echoproject/echo/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +24,20 @@ public class GlobalExceptionHandler {
     Map<String, String> error = new HashMap<>();
     error.put("error", ex.getMessage());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult()
+        .getAllErrors()
+        .forEach(
+            (error) -> {
+              String fieldName = ((FieldError) error).getField();
+              String errorMessage = error.getDefaultMessage();
+              errors.put(fieldName, errorMessage);
+            });
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
   }
 }
