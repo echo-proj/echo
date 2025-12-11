91d1dd31da24715ab28caccdb704bbb525917515
YounesDjelloul <younesdjelloul14@gmail.com>
2025-12-11 16:40:04 +0100

    add `findByDocumentId` method to `DocumentContentRepository`

--- backend/src/main/java/com/echoproject/echo/document/repository/DocumentContentRepository.java
+++ backend/src/main/java/com/echoproject/echo/document/repository/DocumentContentRepository.java
@@ -1,9 +1,12 @@
 package com.echoproject.echo.document.repository;
 
 import com.echoproject.echo.document.models.DocumentContent;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentContentRepository extends JpaRepository<DocumentContent, UUID> {}
+public interface DocumentContentRepository extends JpaRepository<DocumentContent, UUID> {
+  Optional<DocumentContent> findByDocumentId(UUID documentId);
+}
